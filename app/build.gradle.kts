c884aaea84d6ce2604afdb9b19bd7ed8879cde4d
jjwm10625 <choyeongseo950@gmail.com>
2024-10-29 03:22:34 +0900

    #34 [WEEK04] 4주차 과제 완료

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,14 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
 }
+
+val properties =
+    Properties().apply { load(project.rootProject.file("local.properties").inputStream()) }
+
 
 android {
     namespace = "com.gdg.android"
@@ -13,10 +20,11 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
+
+            buildConfigField("String", "BASE_URL", properties["base.url"].toString())
         }
     }
 
@@ -41,6 +49,7 @@ android {
 
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 
     composeOptions {
@@ -57,7 +66,7 @@ android {
 dependencies {
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx) // LiveData를 위한 라이브러리
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
@@ -65,13 +74,35 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    // ViewModel과 Compose 통합
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
+
+    // LiveData KTX
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1")
+
     // Navigation Compose 최신 버전으로 업데이트
     implementation("androidx.navigation:navigation-compose:2.7.0")
+
+
+    // Compose 라이브러리
+    implementation("androidx.compose.runtime:runtime-livedata:1.4.0") // 이 줄을 추가하세요.
+
+    implementation("androidx.compose.material3:material3:1.2.0") // 예시로 적절한 최신 버전으로 변경
+
 
     // 코루틴 라이브러리 추가
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") // 최신 버전으로 업데이트
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3") // 최신 버전으로 업데이트
 
+    // Retrofit2
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+
+    // Test Dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/gdg/android/presentation/UserScreen.kt
@@ -0,0 +1,104 @@
+package com.gdg.android.presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.gdg.android.api.User
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserScreen(navController: NavController) {
+    val mainViewModel: MainViewModel = viewModel()
+    val users by mainViewModel.users.observeAsState(emptyList())
+
+    LaunchedEffect(Unit) {
+        mainViewModel.getUsers()
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
+                },
+                title = {
+                    Text(
+                        text = "유저 목록",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.White)
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(innerPadding)
+        ) {
+            LazyColumn {
+                items(users) { user ->
+                    UserItem(user)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun UserItem(user: User) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp, horizontal = 18.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            Text(
+                text = user.firstName,
+                color = Color.Black
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = user.email,
+                color = Color.Gray
+            )
+        }
+        AsyncImage(
+            modifier = Modifier
+                .size(60.dp)
+                .clip(RoundedCornerShape(12.dp)),
+            model = user.avatar,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
