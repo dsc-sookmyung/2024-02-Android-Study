6d007f5b2611399fa4a43018d253b2bb685d3030
nhyeonii <nhyeonii@naver.com>
2024-11-10 15:39:50 +0900

    #52 [WEEK06] 폰트 및 컬러 사용하여 UI 구성, 디자인 패턴 뷰모델 적용, Hilt 라이브러리 의존성 주입

---
+++ app/src/main/java/com/gdg/android/UserViewModel.kt
@@ -0,0 +1,31 @@
+package com.gdg.android
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class UserViewModel : ViewModel() {
+    private val _users = MutableStateFlow<List<User>>(emptyList())
+    val users: StateFlow<List<User>> get() = _users
+
+    init {
+        getUsers()
+    }
+
+    private fun getUsers() {
+        viewModelScope.launch {
+            runCatching { ServicePool.userService.getUsers(page = 2) }
+                .onSuccess {
+                    _users.value = it.data
+                    Log.d("MainViewModel", "getUsers: ${it.data}")
+                }
+                .onFailure {
+                    _users.value = emptyList()
+                    Log.e("MainViewModel", "getUsers: ${it.message}")
+                }
+        }
+    }
+}
--- app/src/main/java/com/gdg/android/ui/theme/Type.kt
+++ app/src/main/java/com/gdg/android/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.gdg.android.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.gdg.android.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,20 +17,51 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+)
+
+val pretendardBold = FontFamily(Font(R.font.pretendard_bold, FontWeight.Bold))
+val pretendardSemiBold = FontFamily(Font(R.font.pretendard_semibold, FontWeight.SemiBold))
+val pretendardRegular = FontFamily(Font(R.font.pretendard_regular, FontWeight.Normal))
+val pretendardMedium = FontFamily(Font(R.font.pretendard_medium, FontWeight.Medium))
+val kjcBold = FontFamily(Font(R.font.kimjungchulmyungjo_bold, FontWeight.Bold))
+val kjcRegular = FontFamily(Font(R.font.kimjungchulmyungjo_regular, FontWeight.Medium))
+val kjcLight = FontFamily(Font(R.font.kimjungchulmyungjo_light, FontWeight.Light))
+// Button
+val button1Bold = TextStyle(
+    fontFamily = kjcBold,
+    fontSize = 16.sp,
+)
+val button2Bold = TextStyle(
+    fontFamily = kjcBold,
+    fontSize = 14.sp,
+)
+val button3Bold = TextStyle(
+    fontFamily = kjcBold,
+    fontSize = 11.sp,
+)
+
+val text1Light = TextStyle(
+    fontFamily = kjcLight,
+    fontSize = 16.sp
+)
+val text2Light = TextStyle(
+    fontFamily = kjcLight,
+    fontSize = 14.sp
+)
+val text3Lightm = TextStyle(
+    fontFamily = kjcLight,
+    fontSize = 10.sp
+)
+
+val text1Regular = TextStyle(
+    fontFamily = kjcRegular,
+    fontSize = 16.sp
+)
+val text2Regular = TextStyle(
+    fontFamily = kjcRegular,
+    fontSize = 14.sp
+)
+val text3Regular = TextStyle(
+    fontFamily = kjcRegular,
+    fontSize = 10.sp
 )
